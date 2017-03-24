8fc3419dabe31cbff040248424d904de052883be
Avinesh Singh <avin3sh@gmail.com>
2017-03-24 18:23:18 +0530

    Removed ajax from login and made it redirect based

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,6 +3,7 @@ module.exports = function (req, res, Users, Chatroom, cookie) {
 
     var username = req.body.username;
     var pass = req.body.password;
+    console.log("Received username and passs is: "+ username + " , "+ pass);
 
     Users.count({ username: username, password: pass }, function (err, data) {
         if (data === 1) {
@@ -11,13 +12,14 @@ module.exports = function (req, res, Users, Chatroom, cookie) {
             cookie = req.cookies.username;
 
             var loginusername = { uname: username };
-            Chatroom.find({}, function (err, data) {
+            /*Chatroom.find({}, function (err, data) {
                 if (err) throw err;
                 //console.log(data);
                 var crlistejsmetadata = { title: 'TheChat Project' }; //to pass some generic vars to ejs template of chatroom-list
                 res.render('chatroom-list', { meta: crlistejsmetadata, Username: loginusername, chatroomdata: data }); //passing post variable from login page, namely username to the main chatroom page along with db result
 
-            });
+            });*/
+            res.redirect('/');
         } else {
             res.send(`Invalid details! <a href="/">Click here</a> to try again.`);
         }
